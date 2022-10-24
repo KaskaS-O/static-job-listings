b8c12896095cba065b65ac0e46054b2b89bc2ea4
KaskaS-O <kaska.salaban@gmail.com>
2022-10-24 11:15:58 +0200

    Initial commit

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">CodingDuck</a>.
+      </div>
+    </div>
+  );
+}
+
+export default App;
